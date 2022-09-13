ece1befb5b6af13333979855cab0cb8d86e75e05
Winsdtrom5 <anggagant@gmail.com>
2022-09-13 08:29:27 +0700

    Kumpul GD4

---
+++ app/src/main/java/com/example/view_binding_10858/TaskList.kt
@@ -0,0 +1,10 @@
+package com.example.view_binding_10858
+
+object TaskList {
+    val TaskList = listOf<Task>(
+        Task("Chapter 1","Bangun Tidur Dengan Semangat","Dibangunkan Oleh Alarm USSR Athem Membuat Semangat Ketika Sudah Bangun Dari Tidur"),
+        Task("Chapter 2","Share Di Internet","Share Meme Absurd Melalui Facebook Setiap Hari"),
+        Task("Chapter 3","Gacha Blue Archive Sampe Habis Pyroxene","Nguli Pyroxene Sampe Dapat Izuna Swimsuit Walaupun Spark:))"),
+        Task("Chapter 4","Nonton Anime","Nonton Anime Sampe Turu")
+    )
+}
